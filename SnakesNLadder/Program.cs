93c3ae02ce377cc10f981e32f4f9cc69d72ee88f
asmithasatesh <asmithasatesh@gmail.com>
2021-06-28 20:48:13 +0530

    Check options for playing

--- SnakesNLadder/Program.cs
+++ SnakesNLadder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SnakesNLadder
 {
@@ -6,10 +7,28 @@ namespace SnakesNLadder
     {
         static void Main(string[] args)
         {
+            //local variable
             int rollDice,playerPosition = 0;
+
+            //Create random number to roll dice
             Random random = new Random();
             rollDice = random.Next(1, 7);
             Console.WriteLine("Dice number is : " + rollDice);
+
+            //Print user options
+            Console.WriteLine("Player checks options 1.No Play  2.Ladder   3.Snake");
+            var list = new List<string> { "No Play", "Ladder","Snake"};
+            //Use random to check user option
+            int index = random.Next(list.Count);
+            //Conditions for user options
+            if (list[index] == "Ladder") playerPosition += rollDice;
+            if (list[index] == "Snake") playerPosition -= rollDice;
+            //Print to Console
+            Console.WriteLine("Player option is : " + list[index]);
+            Console.WriteLine("\n");
+            Console.WriteLine("Player current position : " + playerPosition);
+
+
         }
     }
 }
